40cbfc980b0f6f3a7479fa6b066b0c05bca58bfe
Hemanthn <hemanthn@pmk.com>
2025-04-02 16:13:32 -0400

    sample problems to work with

---
+++ practice_execises/0-passArguments.sh
@@ -0,0 +1,6 @@
+#!/bin/bash 
+
+echo "The first Argument passed is $1"
+echo "the Second Argument passes is $2"
+
+echo "All the Arguments are: $@"
---
+++ practice_execises/0-userInput.sh
@@ -0,0 +1,6 @@
+# Write a Bash script which accepts  as input and displays the greeting "Welcome (name)"
+
+#!/bin/bash 
+
+read -p "Please enter your Full Name: " fullname
+echo "Welcome $fullname"
